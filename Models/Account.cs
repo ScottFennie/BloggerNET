bd2f9863ac91034514f7ff01b84bce0f4f537e8f
Scott Fennie <s.fennie7@gmail.com>
2021-10-22 13:09:02 -0600

    starting up

---
+++ Models/Account.cs
@@ -0,0 +1,10 @@
+namespace BloggerDotNet.Models
+{
+    public class Account : Profile
+    {
+        public new string Id { get; set; }
+        public new string Name { get; set; }
+        public new string Email { get; set; }
+        public new string Picture { get; set; }
+    }
+}
